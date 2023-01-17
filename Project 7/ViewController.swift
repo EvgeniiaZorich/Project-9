5e8d0c493f652c84d50306b89f64b5d0305ca268
Evgeniia Zorich <vasileva.evg.al@gmail.com>
2023-01-17 11:21:01 +0100

    feature async

--- Project 7/ViewController.swift
+++ Project 7/ViewController.swift
@@ -1,36 +1,34 @@
 //
 //  ViewController.swift
-//  Project 7
+//  Project7_100days
 //
-//  Created by Евгения Зорич on 10.01.2023.
+//  Created by user228564 on 1/11/23.
 //
 
 import UIKit
 
 class ViewController: UITableViewController {
+
     var petitions = [Petition]()
-    var petitionsFiltred = [Petition]()
+    var filtered = [Petition]()
     var keyword: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(showCredits))
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(filterPetitions))
+        title = "The White House Petitions"
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Source", style: .plain, target: self, action: #selector(source))
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterPetitions))
+        
+    
         performSelector(inBackground: #selector(fetchJSON), with: nil)
     }
-    
+
     @objc func fetchJSON() {
-        let urlString: String
-
-        if navigationController?.tabBarItem.tag == 0 {
-            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
-        } else {
-            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
-        }
-
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
@@ -40,70 +38,67 @@ class ViewController: UITableViewController {
 
         performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
     }
-     
+
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            filterData()
+            tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+        } else {
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+        }
+    }
+
+    @objc func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+        
     
-    @objc func showCredits() {
-        let ac = UIAlertController(title: "Credits", message: "The data comes from the We The People API of the Whitehouse", preferredStyle: .alert)
+    @objc func source() {
+        let ac = UIAlertController(title: "Source", message: "Petitions uploaded from  petitions.whitehouse.gov", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
     }
     
-    
     @objc func filterPetitions() {
-        let ac = UIAlertController(title: "Enter word", message: nil, preferredStyle: .alert)
-        ac.addTextField()
         
+        let ac = UIAlertController(title: "Filter", message: "Enter a keyword or leave empty to reset", preferredStyle: .alert)
+        ac.addTextField()
+
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.addAction(UIAlertAction(title: "Filter", style: .default) {
             [weak self, weak ac] _ in
             self?.keyword = ac?.textFields?[0].text ?? ""
             self?.filterData()
             self?.tableView.reloadData()
         })
-                     
+        
         present(ac, animated: true)
+
     }
     
     func filterData() {
         if keyword.isEmpty {
-            petitionsFiltred = petitions
-            title = "The White House Petitions"
+            filtered = petitions
+            
             return
         }
-        
-        title = "Filter: \"\(keyword)\""
-        
-        petitionsFiltred = petitions.filter({$0.title.lowercased().contains(keyword.lowercased()) ||  $0.title.lowercased().contains(keyword.lowercased())})
-        
-//        for i in petitions {
-//            if i.body.lowercased().contains(keyword.lowercased()) || i.title.lowercased().contains(keyword.lowercased()){
-//                petitionsFiltred.append(i)
-//            }
-//        }
-    }
-    
-   @objc func showError() {
-            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            present(ac, animated: true)
-            }
-    
-    func parse(json: Data) {
-        let decoder = JSONDecoder()
 
-        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
-            petitions = jsonPetitions.results
-            tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
-            } else {
-                performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
-        }
+        filtered = petitions.filter({$0.title.lowercased().contains(keyword.lowercased()) ||  $0.title.lowercased().contains(keyword.lowercased())})
     }
     
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitionsFiltred.count
+        return filtered.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitionsFiltred[indexPath.row]
+        let petition = filtered[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -111,12 +106,8 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitionsFiltred[indexPath.row]
+        vc.detailItem = filtered[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
-    
 }
-        
-    
-
 
